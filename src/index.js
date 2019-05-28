fd3af67d167c7bdd1c1d0c7a22f878bf204b5db7
Miriam Hadfield <mhadfield@juniper.net>
2019-05-28 14:27:13 -0700

    add new component and dictionary mapping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import './styles.css';
-const MyComponent = () => (
-    <h1>Hello Miriam from My Component</h1>
-);
-export default MyComponent;
+
+export { default as MyComponent } from './index';
+export { default as MyComponentCopy } from './indexCopy';
